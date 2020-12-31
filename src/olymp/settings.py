61336e4c3fea038bb34600561f90c182f357fbc1
Andrew Gein <andgein@yandex.ru>
2020-12-31 14:15:20 +0300

    Fix env, add variables

--- src/olymp/settings.py
+++ src/olymp/settings.py
@@ -84,7 +84,7 @@ WSGI_APPLICATION = 'olymp.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default': env.db(default={
+    'default': env.db("DATABASE_URL", default={
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     })
